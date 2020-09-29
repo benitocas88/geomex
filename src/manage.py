92ba909a14ea8c91d23a9a37597a98cfb6e198c3
Ebenezer Pool Castillo <beni522@gmail.com>
2020-09-29 17:19:12 -0500

    feat(app): header and small form to find a postal code, base.html and removing test routes

--- src/manage.py
+++ src/manage.py
@@ -3,7 +3,6 @@ from flask.templating import render_template
 from flask.wrappers import Response
 
 from app import create_app
-from geomex.models import State, Municipality
 from geoscript import Geomex
 
 env = getenv('FLASK_ENV', 'development')
@@ -12,23 +11,7 @@ app = create_app(env)
 
 @app.route('/')
 def home_state() -> Response:
-    states = State.query.all()
-    return render_template('geomex/state.html', states=states)
-
-
-@app.route('/state/<int:state_id>')
-def home_municipality(state_id: int) -> Response:
-    state = State.query.get(state_id)
-    return render_template('geomex/municipality.html', state=state)
-
-
-@app.route('/municipality/<int:municipality_id>')
-def home_neighborhood(municipality_id: int) -> Response:
-    municipality = Municipality.query.get(municipality_id)
-    return render_template(
-        'geomex/neighborhood.html',
-        municipality=municipality
-    )
+    return render_template('geomex/zipcode.html')
 
 
 @app.cli.command('geo')
