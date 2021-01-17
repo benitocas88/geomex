23bffedd8e339543196a2d767905328489933366
Ebenezer Pool Castillo <beni522@gmail.com>
2021-01-16 19:02:32 -0600

    removing unsided files and updating packages

--- src/user/views.py
+++
@@ -1,8 +0,0 @@
-from admin.api import Blueprint
-
-user = Blueprint('user', __name__)
-
-
-@user.route('/sign-up', method=['GET'])
-def sign_up():
-    pass
--- src/users/models.py
+++ src/users/models.py
@@ -1,4 +1,4 @@
-from app import db
+from admin.models import db
 
 
 class User(db.Model):
